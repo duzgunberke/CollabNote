844657e03f900c7484164e952896296afdf06554
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-12-20 14:29:57 +0300

    fix

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://key-racer-25.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
